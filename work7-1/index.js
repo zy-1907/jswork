7df01a9f94f5c577dc12429ce509fe283ce2f24c
zy-1907 <571671503@qq.com>
2019-10-22 23:31:41 +0800

    work7-1

--- work7-1/index.js
+++ work7-1/index.js
@@ -8,6 +8,10 @@ function sort(method) {
         logs = insertSort(origin)
     }
      showDiv = document.getElementById('Anim')
-    logValues = logs.values()
-        setTimeout("showLog(logValues,showDiv)",50)
+    //生成排序日志数组的遍历器对象
+     logValues = logs.values()
+    //遍历器对象返回的数据结构
+     console.log(logValues.next())
+     //延时调用showLog，调用一次显示一条排序状态
+     setTimeout("showLog(logValues,showDiv)",50)
 }
