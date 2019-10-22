b7f877a5d282ffe5f23e6a53ae0312676bb9980e
zy-1907 <571671503@qq.com>
2019-10-22 23:31:41 +0800

    add explNtion note

--- work7-1/utils.js
+++ work7-1/utils.js
@@ -1,19 +1,24 @@
 function bubbleSort(str) {
     let arr = str.split(',')
+    //用于动画显示的数组
     let sortLog = []
+    //比较轮数等于数组长度-1轮
     for (let i=0;i<arr.length-1;i++){
+        //从第一个开始比较相邻的两个数值
         for (let j=0;j<arr.length-i-1;j++){
+            //把比较前的数组状态和定位指针保存到sortLog中
             sortLog.push([arr.concat(),[j,j+1]])
-            sortLog.push([arr.concat(),[j,j+1]])
-            sortLog.push([arr.concat(),[j,j+1]])
-
+            //比较相邻的两个元素，大的交换到右边
             if(arr[j]>arr[j+1]){
                 [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+                //比较后的数组状态和定位指针保存到sortLog
                 sortLog.push([arr.concat(),[j,j+1]])
             }
         }
     }
+    //把排序最终状态的数组和取消定位指针保存到sortLog中，因为动画的最后状态是不显示比较数据的
     sortLog.push([arr.concat(),[-1,-1]])
+    //console.log(sortLog)
     return sortLog
 }
 
@@ -35,10 +40,13 @@ function insertSort(str) {
     sortLog.push([arr.concat(),[-1,-1]])
     return sortLog
 }
+    //显示排序每个步骤
 function showLog(logValues,showElement) {
     let str = ''
-    //解构赋值
+    //解构赋值,从排序日志遍历器取出一条结果赋值给row和pos变量
     let {done,value:[row,pos]}=logValues.next()
+    //console.log(row)
+    //console.log(pos)
     for(const key in row){
         let color = pos.indexOf(Number(key))>-1?'color:red;':''
 
@@ -47,6 +55,7 @@ function showLog(logValues,showElement) {
     }
 
     showElement.innerHTML=str
+    //如果定位指针不是-1（最后一条记录），就调用函数显示下一条记录
     if(pos[0]!==-1)
         setTimeout("showLog(logValues,showDiv)",500)
 }
