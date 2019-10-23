5812288c7eaf9c6bc69dcef06d4000abfeeee842
zy-1907 <571671503@qq.com>
2019-10-23 21:01:27 +0800

    work8

--- work8/goldbanch.js
+++ work8/goldbanch.js
@@ -1,40 +1,78 @@
 function goldbanch() {
   var num = document.getElementById('ounum').value
-  if (num % 2 !== 0) {
-    alert("这是奇数！")
-  }else{
-    isTrue(num);
-  }
-
-  // 判断一个数是否是质数
-  function factorial(a) {
-    var sum = 0;
-    for (var i = 1; i <= a; i++) {
-      if (a % i == 0) {
-        sum++;
-      }
-    }
-    if (sum == 2) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  // 判断用户输入的偶数是否可分为两个质数之和：
-
-  function isTrue(a) {
-    var arr = []
-    var str = ""
-    for (var i = 2; i < a - 2; i++) {
-      var j = a - i;
-      if (factorial(i) && factorial(j)) {
-        arr.push("<div>" + a + "可以拆分为两个质数" + i + "与" + j + "的和" + "</div>");
-
-      }
-    }
-    for (var i = 0; i < arr.length; i++) {
-      str += arr[i];
-      show.innerHTML = str
-    }
+  if (num <= 2 || num % 2 !== 0) {
+    alert("请输入一个大于2的偶数")
+  } else {
+    find(num)
   }
 }
+
+function isPrime(number) {
+
+  var count = 0;
+
+  for (var i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      count++;
+    }
+  }
+
+  if (count == 2) {
+    return true;
+  }
+}
+
+function find(n) {//输入8
+
+  var arr = []
+  for (var i = 1; i < n; i++) { //i=1 1<=8 y=7 //i=2 2<=8 y=6 //i=3 3<=8 y=5 true
+    //i=4 4<=8 y=4 //i=5 5<=8 y=3 true //i=6 6<=8 y=2
+    //i=7 7<=8 y=1 //i=8 8<=8 y=0
+    var y = n - i;
+
+
+    if (isPrime(y) && isPrime(i)) {
+      arr.push([y, i])  //因为会重复输出所以把素数加到数组，通过比较把重复的去掉
+    }
+  }
+
+            //通过比较去掉重复的输出
+                /*[0][0]=[1][1]
+                  [0][1]=[1][0]
+
+                  [1][0]=[2][1]
+                  [1][1]=[2][0]
+
+                  [2][0]=[3][1]
+                  [2][1]=[3][0]
+                */
+
+  for (let bj = 1; bj < arr.length; bj++) {
+   if ((arr[bj - 1][0] == arr[bj][1])) { 
+     if ((arr[bj - 1][1] == arr[bj][0])) {
+
+        //console.log(bj)
+        arr.splice(bj) //返回无重复的新数组
+        
+      }
+    }
+    
+  }
+  
+      var str =""
+      for (var i = 0; i < arr.length; i++) {
+       // console.log( arr[i])
+        str+="<div>"+n+"可以被拆分为两个素数"+arr[i][0]+"和"+arr[i][1]+"的和"+"</div>" 
+     }
+  show.innerHTML = str
+}
+
+
+
+
+
+
+
+
+
+
