02f8aca08139e3816e9e62317ad9ede1912a18ff
zy-1907 <571671503@qq.com>
2019-10-24 23:01:32 +0800

    work8

--- work8/goldbanch.js
+++ work8/goldbanch.js
@@ -35,8 +35,7 @@ function find(n) {//输入8
       arr.push([y, i])  //因为会重复输出所以把素数加到数组，通过比较把重复的去掉
     }
   }
-
-            //通过比较去掉重复的输出
+//通过比较去掉重复的输出
                 /*[0][0]=[1][1]
                   [0][1]=[1][0]
 
@@ -46,19 +45,28 @@ function find(n) {//输入8
                   [2][0]=[3][1]
                   [2][1]=[3][0]
                 */
+  for(let bj=1;bj<arr.length;bj++){
+                 
+    if((arr[bj-1][0] == arr[bj][1]||arr[bj][0]==arr[bj][1])){
+      if((arr[bj-1][1] ==arr[bj][0])||arr[bj][0]==arr[bj][1]){
 
-  for (let bj = 1; bj < arr.length; bj++) {
-   if ((arr[bj - 1][0] == arr[bj][1])) { 
-     if ((arr[bj - 1][1] == arr[bj][0])) {
+        if(arr[bj][0]==arr[bj][1]){
+          /*当输入26时 0:  (2) [23, 3]
+                       1: (2) [19, 7]
+                       2: (2) [13,13]
+                       3: (2) [7, 19]
+                       4: (2) [3, 23]*/
+          arr.splice(bj+1)
+        }else{
 
-        //console.log(bj)
-        arr.splice(bj) //返回无重复的新数组
-        
+          //console.log(bj)
+          arr.splice(bj)//返回无重复的新数组
+        }
       }
+      
     }
     
   }
-  
       var str =""
       for (var i = 0; i < arr.length; i++) {
        // console.log( arr[i])
