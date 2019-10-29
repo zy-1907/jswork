117ea9242d98839b325c011c8c5551fdccf922fa
zy-1907 <571671503@qq.com>
2019-10-29 17:33:59 +0800

    work10

---
+++ work10/index.js
@@ -0,0 +1,37 @@
+function monkey() {
+    let total = document.getElementById('monkeytotal').value
+    let kick = document.getElementById('monkeykick').value
+    total = parseInt(total) && Number(total)
+    kick = parseInt(kick) && Number(kick)
+    if (isNaN(total) || isNaN(kick)) {
+        alert('请输入数字')
+        return
+    }
+    let monkey = []
+    for (let i = 1; i <= total; i++) {
+        monkey.push(i)
+    }
+    let i = 0
+    while (monkey.length > 1) {
+        i++
+        head = monkey.shift()
+        if (i % kick != 0) {
+            monkey.push(head)
+        }
+    }
+    document.getElementById('monkeyking').innerHTML = monkey[0]
+}
+function stat(){
+   let str = document.getElementById('str').value
+   let obj = {}
+   let n = str.split("");
+   for(i=0;i<n.length;i++){
+       key = n[i];
+       if(obj[key]){
+           obj[key]++;
+       }else{
+           obj[key]=1;
+       }
+   }
+    document.getElementById('result').innerHTML = JSON.stringify(obj)
+}
