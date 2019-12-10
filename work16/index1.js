9f271d89a1cbeea8644e65a2436a6c18844f189e
zy-1907 <571671503@qq.com>
2019-12-10 15:56:12 +0800

    work16

---
+++ work16/index1.js
@@ -0,0 +1,39 @@
+function animate(obj, option) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        var flag = true;
+        for (var k in option) {
+            var leader = parseInt(getStyle(obj,k)) || 0;
+            var target = option[k];
+            console.log(target)
+            var step = (target - leader) / 10;
+            step = step > 0 ? Math.ceil(step):Math.floor(step);
+            leader = leader + step;
+            obj.style[k] = leader + 'px';
+            if (leader != target) {
+                flag = false;
+            }
+
+        }
+        if (flag) {
+            clearInterval(obj.timer);
+        }
+
+    }, 15);
+}
+function getStyle(obj, attr) {
+    if (window.getComputedStyle) {
+        return window.getComputedStyle(obj, null)[attr];
+    } else {
+        return obj.currentStyle[attr];
+    }
+}
+
+var obj = document.getElementById('box');
+
+obj.onclick = function () {
+    var left=parseInt(Math.random()*300)
+    var top=parseInt(Math.random()*300)
+    animate(obj, { left: left, top: top });
+    
+}
